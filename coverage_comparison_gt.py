385d4c082cb64c3f41354dfe127986bb5dbb0996
Juan Manuel Fleitas Guevara <145216141+Juanmfleitas@users.noreply.github.com>
2025-04-20 00:21:00 -0500

    Update coverage_comparison_gt.py

--- coverage_comparison_gt.py
+++ coverage_comparison_gt.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 from shapely.wkt import loads
+from shapely.wkb import loads as load_wkb
 
 import numpy as np
 import plotly.express as px  # (version 4.7.0 or higher)
@@ -22,21 +23,60 @@ app.layout = html.Div([
 
     html.H1("Comparación Cobertura Móvil Tigo y Claro - Guatemala. Periodo: Octubre 2024 a Marzo 2025", style={'text-align': 'center'}),
 
-    dcc.Dropdown(
-        id="slct_tech",
-        options=[
-            {"label": "5G", "value": 'NR'},
-            {"label": "4G", "value": 'LTE'},
-            {"label": "WCDMA", "value": 'WCDMA'},
-            {"label": "CDMA", "value": 'CDMA'},
-            {"label": "GSM", "value": 'GSM'}
-        ],
-        multi=False,
-        value='LTE',
-        style={'width': "40%"}
-    ),
+    html.Div([
+        dcc.Dropdown(
+            id="slct_tech",
+            options=[
+                {"label": "5G", "value": 'NR'},
+                {"label": "4G", "value": 'LTE'},
+                {"label": "WCDMA", "value": 'WCDMA'},
+                {"label": "CDMA", "value": 'CDMA'},
+                {"label": "GSM", "value": 'GSM'}
+            ],
+            multi=False,
+            value='NR',
+            style={'width': "40%", 'margin-right': '20px'}
+        ),
 
-    html.Div(id='output_container', children=[]),
+        dcc.Dropdown(
+            id="slct_adm1",
+            options=[
+                {"label": "Huehuetenango", "value": 'Huehuetenango'},
+                {"label": "Petén", "value": 'Petén'},
+                {"label": "Alta Verapaz", "value": 'Alta Verapaz'},
+                {"label": "Quiché", "value": 'Quiché'},
+                {"label": "San Marcos", "value": 'San Marcos'},
+                {"label": "Quetzaltenango", "value": 'Quetzaltenango'},
+                {"label": "Totonicapán", "value": 'Totonicapán'},
+                {"label": "Sololá", "value": 'Sololá'},
+                {"label": "Retalhuleu", "value": 'Retalhuleu'},
+                {"label": "Suchitepéquez", "value": 'Suchitepéquez'},
+                {"label": "Chimaltenango", "value": 'Chimaltenango'},
+                {"label": "Guatemala", "value": 'Guatemala'},
+                {"label": "Sacatepéquez", "value": 'Sacatepéquez'},
+                {"label": "Baja Verapaz", "value": 'Baja Verapaz'},
+                {"label": "El Progreso", "value": 'El Progreso'},
+                {"label": "Jalapa", "value": 'Jalapa'},
+                {"label": "Escuintla", "value": 'Escuintla'},
+                {"label": "Santa Rosa", "value": 'Santa Rosa'},
+                {"label": "Jutiapa", "value": 'Jutiapa'},
+                {"label": "Izabal", "value": 'Izabal'},
+                {"label": "Zacapaz", "value": 'Zacapa'},
+                {"label": "Chiquimula", "value": 'Chiquimula'}
+            ],
+            multi=False,
+            value='Guatemala',
+            style={'width': "40%"}
+        )
+    ], style={'display': 'flex', 'justify-content': 'center'}),
+
+
+    html.Div([
+        html.Div(id='output_container', children=[], style={'width': "40%", 'margin-right': '20px'}),
+        html.Div(id='output_container1', children=[], style={'width': "40%"}),
+    
+    ], style={'display': 'flex', 'justify-content': 'center'}),
+    
     html.Br(),
 
     dcc.Graph(id='my_tech_map', figure={})
@@ -49,35 +89,42 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 @app.callback(
     [Output(component_id='output_container', component_property='children'),
+     Output(component_id='output_container1', component_property='children'),
      Output(component_id='my_tech_map', component_property='figure')],
 #     Output(component_id='bar_plot', component_property='figure')],
-    [Input(component_id='slct_tech', component_property='value')]
+    [Input(component_id='slct_tech', component_property='value'),
+     Input(component_id='slct_adm1', component_property='value')]
 
     )
 
-def update_graph(option_slctd):
+
+def update_graph(option_slctd, option_adm1):
 
 # ------------------------------------------------------------------------------
 # Leer parquet procesado
-    columns_needed = ['quadkey', 'geometry', 'technology','comparison']
+    columns_needed = ['quadkey', 'geometry', 'technology','comparison','ADM1_ES']
     pivot_table = pd.read_parquet("processed_data.parquet", columns=columns_needed)
     #pivot_table = pd.read_parquet("processed_data.parquet")
 
     # Convertir geometría WKT a objeto geométrico
-    pivot_table['geometry'] = pivot_table['geometry'].apply(loads)
+    pivot_table['geometry'] = pivot_table['geometry'].apply(load_wkb)
 
     # Crear GeoDataFrame
     gdf_csv = gpd.GeoDataFrame(pivot_table, geometry="geometry")
-
+    
     if gdf_csv.crs is None:
         gdf_csv.set_crs(epsg=4326, inplace=True)
 
     print(option_slctd)
+    print(option_adm1)
 
     container = f"The selected technology was: {option_slctd}"
+    container1 = f"The selected region was: {option_adm1}"
 
     # Filtrar GeoDataFrame por tecnología
     dff = gdf_csv[gdf_csv["technology"] == option_slctd]
+    dff = dff[dff["ADM1_ES"] == option_adm1]
+
 
     if dff.empty:
         print("GeoDataFrame is empty.")
@@ -117,7 +164,7 @@ def update_graph(option_slctd):
         margin={"r": 0, "t": 0, "l": 0, "b": 0}
     )
 
-    return container, fig
+    return container, container1, fig
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
