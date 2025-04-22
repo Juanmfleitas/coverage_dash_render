8f0ba8258e9f0603146058b3d7b2947ea654da65
Juan Manuel Fleitas Guevara <145216141+Juanmfleitas@users.noreply.github.com>
2025-04-22 13:00:39 -0500

    Update coverage_comparison_gt.py

--- coverage_comparison_gt.py
+++ coverage_comparison_gt.py
@@ -21,22 +21,22 @@ server = app.server
 # App layout
 app.layout = html.Div([
 
-    html.H1("Tigo and Claro 5G Mobile Coverage Comparison - Guatemala. Period: December 2024", style={'text-align': 'center'}),
-
+    html.H1("Comparación Cobertura Móvil Tigo y Claro - Guatemala. Periodo: Diciembre 2024", style={'text-align': 'center'}),
+ 
     html.Div([
-        dcc.Dropdown(
-            id="slct_tech",
-            options=[
-                {"label": "5G", "value": 'NR'},
-                {"label": "4G", "value": 'LTE'},
-                {"label": "WCDMA", "value": 'WCDMA'},
-                {"label": "CDMA", "value": 'CDMA'},
-                {"label": "GSM", "value": 'GSM'}
-            ],
-            multi=False,
-            value='LTE',
-            style={'width': "40%", 'margin-right': '20px'}
-        ),
+    #     dcc.Dropdown(
+    #         id="slct_tech",
+    #         options=[
+    #             {"label": "5G", "value": 'NR'},
+    #             {"label": "4G", "value": 'LTE'},
+    #             {"label": "WCDMA", "value": 'WCDMA'},
+    #             {"label": "CDMA", "value": 'CDMA'},
+    #             {"label": "GSM", "value": 'GSM'}
+    #         ],
+    #         multi=False,
+    #         value='LTE',
+    #         style={'width': "40%", 'margin-right': '20px'}
+    #     ),
 
         dcc.Dropdown(
             id="slct_adm1",
@@ -66,16 +66,17 @@ app.layout = html.Div([
             ],
             multi=False,
             value='Guatemala',
+            #style={'width': "40%"}
             style={'width': "40%"}
         )
-    ], style={'display': 'flex', 'justify-content': 'center'}),
+    ], style={'display': 'flex' }), #'justify-content': 'center'
 
 
     html.Div([
-        html.Div(id='output_container', children=[], style={'width': "40%", 'margin-right': '20px'}),
+ #       html.Div(id='output_container', children=[], style={'width': "40%", 'margin-right': '20px'}),
         html.Div(id='output_container1', children=[], style={'width': "40%"}),
     
-    ], style={'display': 'flex', 'justify-content': 'center'}),
+    ], style={'display': 'flex'}),  #, 'justify-content': 'center'
     
     html.Br(),
     dcc.Graph(id='my_tech_map', figure={}),
@@ -90,15 +91,16 @@ app.layout = html.Div([
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 @app.callback(
-    [Output(component_id='output_container', component_property='children'),
+    [#Output(component_id='output_container', component_property='children'),
      Output(component_id='output_container1', component_property='children'),
      Output(component_id='my_tech_map', component_property='figure'),
      Output(component_id='comparison_bar_chart', component_property='figure')],  # Nuevo output
-    [Input(component_id='slct_tech', component_property='value'),
+    [#Input(component_id='slct_tech', component_property='value'),
      Input(component_id='slct_adm1', component_property='value')]
+
     )
 
-def update_graph(option_slctd, option_adm1):
+def update_graph(option_adm1): #option_slctd, 
 
 # ------------------------------------------------------------------------------
 # Leer parquet procesado
@@ -106,8 +108,10 @@ def update_graph(option_slctd, option_adm1):
     pivot_table = pd.read_parquet("processed_data.parquet", columns=columns_needed)
     #pivot_table = pd.read_parquet("processed_data.parquet")
 
-    # Convertir geometría WKT a objeto geométrico
     pivot_table['geometry'] = pivot_table['geometry'].apply(load_wkb)
+    
+    # Convertir geometría WKT a objeto geométrico
+#    pivot_table['geometry'] = pivot_table['geometry'].apply(loads)
 
     # Crear GeoDataFrame
     gdf_csv = gpd.GeoDataFrame(pivot_table, geometry="geometry")
@@ -115,20 +119,19 @@ def update_graph(option_slctd, option_adm1):
     if gdf_csv.crs is None:
         gdf_csv.set_crs(epsg=4326, inplace=True)
 
-    print(option_slctd)
+#    print(option_slctd)
     print(option_adm1)
 
-    container = f"The selected technology was: {option_slctd}"
+#    container = f"The selected technology was: {option_slctd}"
     container1 = f"The selected region was: {option_adm1}"
 
     # Filtrar GeoDataFrame por tecnología
-    dff = gdf_csv[gdf_csv["technology"] == option_slctd]
-    dff = dff[dff["ADM1_ES"] == option_adm1]
-
+ #   dff = gdf_csv[gdf_csv["technology"] == option_slctd]
+    dff = gdf_csv[gdf_csv["ADM1_ES"] == option_adm1]
 
     if dff.empty:
         print("GeoDataFrame is empty.")
-        return container, go.Figure()
+        return container1, go.Figure()  #cambiar---------------------
 
     # Convertir GeoDataFrame a GeoJSON
     geojson = json.loads(dff.to_json())
@@ -158,6 +161,12 @@ def update_graph(option_slctd, option_adm1):
         }
     )
 
+    # fig_map.update_layout(
+    #     width=1700,
+    #     height=700,
+    #     margin={"r": 0, "t": 0, "l": 0, "b": 0}
+    #)
+
     fig_map.update_layout(
     width=1700,
     height=700,
@@ -166,11 +175,13 @@ def update_graph(option_slctd, option_adm1):
         x=0.90,          # horizontal position (0 = left, 1 = right)
         y=0.98,         # vertical position (0 = bottom, 1 = top)
         bgcolor='white',  # Fondo blanco sólido
+#        bgcolor='rgba(255,255,255,0.6)',  # semi-transparent background
         bordercolor='black',
         borderwidth=1,
         font=dict(size=12)
     )
     )
+
 
     # Gráfico de una sola barra apilada (100%)
     bar_data = dff['comparison'].value_counts(normalize=True).mul(100).round(2)
@@ -198,15 +209,16 @@ def update_graph(option_slctd, option_adm1):
 
     bar_fig.update_layout(
         barmode='stack',
-        title='Percentage distribution by available mobile operator',
+        title='Distribución porcentual por operador móvil disponible',
         yaxis=dict(showticklabels=False),
         height=200,
         margin=dict(t=40, b=20, l=20, r=20),
         showlegend=False
     )
 
-    return container, container1, fig_map, bar_fig
+
+    return container1, fig_map, bar_fig #container, 
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=10000)
+#    app.run_server(debug=True)
