e5b569c3bbe029933f8f36395c09503cf6c15bab
Juan Manuel Fleitas Guevara <145216141+Juanmfleitas@users.noreply.github.com>
2025-04-22 13:57:31 -0500

    Update coverage_comparison_gt.py

--- coverage_comparison_gt.py
+++ coverage_comparison_gt.py
@@ -221,4 +221,4 @@ def update_graph(option_adm1): #option_slctd,
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-#    app.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0", port=10000)
